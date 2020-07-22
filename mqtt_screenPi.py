f46201e0e83757e13c3c31f3891e58436783c298
GuillermoElectrico <guillermoelectrico@outlook.com>
2020-07-22 17:17:22 +0200

    Final code (functional)

--- mqtt_screenPi.py
+++ mqtt_screenPi.py
@@ -36,9 +36,6 @@ class DataCollector:
                 log.warning('Failed to re-load topics, going on with the old one.')
                 log.warning(e)
         return self.topics_map
-        
-#    def my_map(x, in_min, in_max, out_min, out_max):
-#        return int((x-in_min) * (out_max-out_min) / (in_max-in_min) + out_min)
 
     def on_message(self, client, userdata, message):
 
@@ -73,7 +70,9 @@ class DataCollector:
         for topics_action in topics:
             if topics_action['topic'] == message.topic:
                 if topics_action['name'] == 'brightness':
-                    backlight.brightness = int((message.topic-100) * (10-100) / (30000-100) + 10)
+                    if stored_message < 3
+                        stored_message == 3
+                    backlight.brightness = stored_message
                 elif topics_action['name'] == 'power':
                     if stored_message == 1:
                         backlight.power = False
